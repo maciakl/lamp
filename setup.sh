e11eb9a7f07f35d9466db12d3c62db5df8b3f5bf
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2013-11-13 12:42:20 -0500

    Color output.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+S='\e[0;34m'
+E='\e[0m'
 
 # Setup unattended access for mysql-server
-echo "Installing LAMP stack"
+echo -e "${S}Installing the LAMP stack...${E}"
 export DEBIAN_FRONTEND=noninteractive
 sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password password toor'
 sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password_again password toor'
 sudo apt-get -y update && sudo apt-get -y -q install apache2 php5 php5-mysql mysql-server mysql-client
 
 # install nonessential stuff
-echo "Installing non-essential packages"
+echo -e "${S}Installing the non-essential packages...${E}"
 sudo apt-get -y -q install vim curl php-pear
 
 # install phpunit and phpdocumentor
-echo "Installing PHPUnit and PHPDocumentor"
+echo -e "${S}Installing PHPUnit and PHPDocumentor...${E}"
 sudo pear channel-update pear.php.net
 sudo pear upgrade-all
 sudo pear config-set auto_discover 1
@@ -22,14 +24,14 @@ sudo pear channel-discover pear.phpdoc.org
 sudo pear install phpdoc/phpDocumentor
 
 # enable mod_rewrite
-echo "Reconfiguring Apache"
+echo -e "${S}Reconfiguring Apache... Please ignore any hostname errors here...${E}"
 sudo a2enmod rewrite
 sudo service apache2 restart
 
 # install Composer
-echo "Installing composer"
+echo -e "${S}Installing Composer package manager...${E}"
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "Done."
+echo -e "${S}Done.${E}"
 echo "You should now be able to vagrant ssh into the box."
